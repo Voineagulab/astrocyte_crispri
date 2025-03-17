1c61d481fc98f70a0e8d616fb845d03f58da92e0
Voineagulab <ivlabunsw@gmail.com>
2025-03-17 13:48:23 +1100

    Update MAGMA_EGrf_run.sh

--- 6_PredictiveModels/2.Scripts/MAGMA_EGrf_run.sh
+++ 6_PredictiveModels/2.Scripts/MAGMA_EGrf_run.sh
@@ -1,4 +1,5 @@
-#Final Background w = 1,0
+#This script Run MAGMA enrichment analyses in EGrf enhancers
+
 ##This part Run MAGMA annotation step and Gene level analyses. The MAGMA annotation step  was executed using a 1-kb window around each region.
 #The default SNP-wise mean model was used for gene level analyses
 Rscript RunMAGMA.R -a -l "" -c 3 -d General_Background -w 1 -b /mnt/Data0/PROJECTS/CROPSeq/Manuscript/GitHub/Scripts/6.Predictive_models/MAGMA/EGrf/ --gene-analysis --input="General_Background"
