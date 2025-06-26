f2119c39639d7f41d33217467b523d35013f401f
Voineagulab <ivlabunsw@gmail.com>
2025-06-26 14:58:06 +1000

    Update WGCNAenh.R

--- 5_FunctionalAnnotation/WGCNAenh.R
+++ 5_FunctionalAnnotation/WGCNAenh.R
@@ -14,9 +14,8 @@ rm(list=ls())
 res=read.csv("/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/FullScale/Results/2_DE/Enh/Results Final.csv")
 hits=res[res$HitPermissive, ]
 nonhits=res[!res$HitPermissive,]
-# load("/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/FullScale/Results/3_HitEnrichment/Chromatin/Coverage/Herring - Intersections and Coverage.rda")
-# data=as.data.frame(herring$CoverageMean)
 
+#### Load candidate enhancer coverage using data from snATAC-seq from Herring et al.
 load("/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/FullScale/Results/3_HitEnrichment/Chromatin/Coverage/Herring - Intersections and Coverage.rda")
 data<- as.data.frame(log2(herring$CoverageMean_Pooled+0.1)) # data from herring et all only has coverage data for 974 of the 979 enhancers
 
@@ -39,7 +38,8 @@ data=data[, sorted.ct$colnames.data.]
 dat=t(data)
 elim=which(apply(dat,2,var)==0) # filtering out one enhancer that has 0 coverage across all samples
 dat=dat[,-elim]
-###WGCNA
+
+### WGCNA
 
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
 sft = pickSoftThreshold(dat, powerVector = powers, verbose = 5, blockSize=40000)
@@ -47,16 +47,14 @@ sft = pickSoftThreshold(dat, powerVector = powers, verbose = 5, blockSize=40000)
 pdf("Soft Thresholding.ATAC.pdf")
 
 par(mfrow = c(1,2));cex1 = 0.9;
-
 plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",main = paste("Scale independence"));
 text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], labels=powers,cex=cex1,col="red");abline(h=0.85,col="red")
-
 plot(sft$fitIndices[,1], sft$fitIndices[,5],xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",main = paste("Mean connectivity"))
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
 
 dev.off()
 
-#---- One step network construction
+#---- One step network construction using power=10
 net=blockwiseModules(dat,
                      power=10,
                      numericLabels=TRUE , 
@@ -76,6 +74,7 @@ modules=as.data.frame(table(net$colors)); colnames(modules)=c("Label", "nGenes")
 modules$Color=c("grey",labels2colors(modules$Label[-1]))
 modules$Label=paste("ME", modules$Label, sep="")
 modules=modules[order(modules$nGenes, decreasing = TRUE) , ]
+
 write.csv(modules, "modules.csv")
 
 moduleLabel=paste("ME",net$colors, sep="")
@@ -90,6 +89,7 @@ write.csv(me, "ME.csv")
 KMEs<-signedKME(dat, net$MEs,outputColumnName = "M")
 kme=data.frame( moduleColor,moduleLabel, KMEs)
 rownames(kme)=rownames(KMEs)
+
 write.csv(kme, "kME.csv")
 
 #---- Calculate stats
@@ -101,20 +101,20 @@ rownames(stats)=c("p.anova.ct", "p.anova.stage", "p.w.astro", "rho.astroStage", 
 m=match(rownames(me), sorted.ct$colnames.data.)
 for (j in c(1:ncol(me))) 
 {
-l=lm(me[,j] ~  sorted.ct$stage[m] + sorted.ct$ct[m])
-a=anova(l)
-stats["p.anova.stage",j]=a$`Pr(>F)`[1]
-stats["p.anova.ct",j]=a$`Pr(>F)`[2]
-w=wilcox.test(me[,j] ~ samples$Astro)
-stats["p.w.astro",j]=w$p.value
-rho=cor.test(me[samples$Astro,j] ,  samples$orderS[samples$Astro], method="s")
-stats["rho.astroStage",j]=rho$estimate
-stats["p.rho.astroStage",j]=rho$p.value
+  l=lm(me[,j] ~  sorted.ct$stage[m] + sorted.ct$ct[m])
+  a=anova(l)
+  stats["p.anova.stage",j]=a$`Pr(>F)`[1]
+  stats["p.anova.ct",j]=a$`Pr(>F)`[2]
+  w=wilcox.test(me[,j] ~ samples$Astro)
+  stats["p.w.astro",j]=w$p.value
+  rho=cor.test(me[samples$Astro,j] ,  samples$orderS[samples$Astro], method="s")
+  stats["rho.astroStage",j]=rho$estimate
+  stats["p.rho.astroStage",j]=rho$p.value
 }
 write.csv(stats, "moduleStats.csv")
 
 
-#-----Plots
+# ###################### Plots
 colheat=colorRampPalette(c("#B51A00", "#FFE2D6","white"))
 colct <- c("#6F083D", "#F4EAD4","#F3CCDE","#3A5F9A","#A7D2DA")
 names(colct) <- unique(samples$ct)
@@ -122,47 +122,50 @@ colstage <- brewer.pal(n = 6, name = "BuPu")
 names(colstage) <- samples$stage[match(c(1:6), samples$orderS)]
 ann_colors <- list(ct = colct, stage = colstage)
 
-###################### Exploratory module plots
+#### Exploratory module plots
+# 
+# pdf("moduleEigengenePlots.pdf", height=10, width=15)
+# for (j in c(1:ncol(me)))
+# {
+#   subtext=paste("p.anova.ct:", round(stats["p.anova.ct",j],5), ";",
+#                 "p.anova.stage:",round(stats["p.anova.stage",j],5), ";" ,
+#                 "p.w.astro", round(stats["p.w.astro",j],5), ";",
+#                 "rho.astroStage", round(stats["rho.astroStage",j],5), ";",
+#                 "p.rho.astroStage",round(stats["p.rho.astroStage",j],5)
+#   )
+#   
+#   colors=labels2colors(sorted.ct$ct[m])
+#   n=modules$nGenes[match(colnames(me)[j] , modules$Label)]
+#   par(las = 2)
+#   par(mar = c(10,0,5,0))
+#   barplot(me[,j], names.arg=rownames(me),
+#           col=colors,
+#           main=paste0("Module:", as.character(colnames(me)[j]), "; NEnh=",n, ";",subtext),
+#           cex.names=1)
+#   e=rownames(kme)[which(kme$moduleLabel%in%colnames(me)[j])]
+#   print(pheatmap(as.matrix(data[e,]), color=rev(colheat(20)),
+#                  cluster_cols = FALSE,gaps_col=grep("Adult", samples$stage[match(colnames(data), rownames(samples))]),
+#                  main=colnames(me)[j]))
+#   
+#   #grid.newpage()
+# }
+# dev.off()
 
-pdf("moduleEigengenePlots.pdf", height=10, width=15)
-for (j in c(1:ncol(me)))
-{
-subtext=paste("p.anova.ct:", round(stats["p.anova.ct",j],5), ";",
-              "p.anova.stage:",round(stats["p.anova.stage",j],5), ";" ,
-              "p.w.astro", round(stats["p.w.astro",j],5), ";",
-               "rho.astroStage", round(stats["rho.astroStage",j],5), ";",
-              "p.rho.astroStage",round(stats["p.rho.astroStage",j],5)
-              )
-
-colors=labels2colors(sorted.ct$ct[m])
-n=modules$nGenes[match(colnames(me)[j] , modules$Label)]
-par(las = 2)
-par(mar = c(10,0,5,0))
-barplot(me[,j], names.arg=rownames(me),
-        col=colors,
-        main=paste0("Module:", as.character(colnames(me)[j]), "; NEnh=",n, ";",subtext),
-        cex.names=1)
-e=rownames(kme)[which(kme$moduleLabel%in%colnames(me)[j])]
-print(pheatmap(as.matrix(data[e,]), color=rev(colheat(20)),
-          cluster_cols = FALSE,gaps_col=grep("Adult", samples$stage[match(colnames(data), rownames(samples))]),
-          main=colnames(me)[j]))
-
-#grid.newpage()
-}
-dev.off()
-
-###################### Figure plots
+#### Figure plots
 # Sort the kme data by module assignment
 kme_sorted=kme[order(kme$moduleLabel),]
 # Within each module, sort data by the kme values of the respective module. 
 for (j in c(3:ncol(kme_sorted)))
 {
-r=grep(colnames(kme_sorted)[j], gsub("ME", "M", kme_sorted$moduleLabel))
-i=order(kme_sorted[r,j], decreasing = TRUE)
-kme_sorted[r,]=kme_sorted[r[i], ]
+  r=grep(colnames(kme_sorted)[j], gsub("ME", "M", kme_sorted$moduleLabel))
+  i=order(kme_sorted[r,j], decreasing = TRUE)
+  kme_sorted[r,]=kme_sorted[r[i], ]
 }
 kme_sorted=kme_sorted[-grep("ME0", kme_sorted$moduleLabel), ]
-data_sorted=data[match(rownames(kme_sorted), rownames(data)) ,]
+data_sorted=data[match(rownames(kme_sorted), rownames(data)) ,] 
+
+# SOURCE DATA
+write.csv(data_sorted, "/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/Manuscript/SourceData/SourceData_Fig1F.csv")
 
 ann=kme_sorted[, c(2,1)]
 
@@ -179,7 +182,7 @@ print(pheatmap(as.matrix(data_sorted),
                gaps_col=grep("Adult", samples$stage[match(colnames(data), rownames(samples))]), 
                show_rownames=FALSE, show_colnames=FALSE,
                annotation_row = ann, annotation_col = samples[, c("ct","stage")], annotation_colors = ann_colors
-                ))
+))
 grid.newpage() # this is needed for pheatmap to work nicely with pdf()
 dev.off()
 
@@ -208,9 +211,14 @@ dev.off()
 # ))
 # grid.newpage() 
 # dev.off()
+
 #plot pvalues only (remove rho) and remove the ME0 grey module.
 splot=data.frame(t(stats[-which( rownames(stats)%in%"rho.astroStage"), -grep("ME0", colnames(stats))]))
 splot=-log10(splot)
+
+# SOURCE DATA
+write.csv(splot, "/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/Manuscript/SourceData/SourceData_Fig1G.csv")
+
 for(j in c(1:ncol(splot))) splot[which(splot[,j]< (-log10(0.05))), j]=0
 
 color_ramp <- colorRamp(c("white", "midnightblue"))
@@ -258,8 +266,8 @@ stats=data.frame(stats, NA, NA); colnames(stats)=names
 
 for(j in c(1:nrow(stats)))
 {
-stats$p.wilcox.hits[j]=wilcox.test(kme[, rownames(stats)[j]] ~ kme$Hit)$p.value
-stats$p.wilcox.hits_vsPowered[j]=wilcox.test(kme_powered[, rownames(stats)[j]] ~ kme_powered$Hit)$p.value
+  stats$p.wilcox.hits[j]=wilcox.test(kme[, rownames(stats)[j]] ~ kme$Hit)$p.value
+  stats$p.wilcox.hits_vsPowered[j]=wilcox.test(kme_powered[, rownames(stats)[j]] ~ kme_powered$Hit)$p.value
 }
 write.csv(t(stats), "moduleStats_Hits.csv")
 
@@ -380,4 +388,3 @@ ggplot(data, aes(x = pval_Stage, y = Count, fill = pval_CT)) +
        y = "Frequency",
        fill = "pval_CT > -log10(0.05)") +
   theme_minimal()
-
