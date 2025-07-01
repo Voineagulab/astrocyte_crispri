b4d6d33c3f17089d74f619bffcd2bc750c96974b
Voineagulab <ivlabunsw@gmail.com>
2025-07-01 12:42:47 +1000

    Update 2e_dCas9-KRAB.R

--- 2_NHACharacterisation_CandidateEnhancerSelection/2e_dCas9-KRAB.R
+++ 2_NHACharacterisation_CandidateEnhancerSelection/2e_dCas9-KRAB.R
@@ -19,7 +19,7 @@ x=data$NHA_2019_GRE13789A15_22KTVVLT3
 # RNA-seq from NHAs transduced with dCas9-KRAB
 y=data$NHA_2019_dCas9_KRAB_GRE13789A13_22KTVVLT3
 
-#Scatterplot shown in SFig1
+#Scatterplot shown in Extended Data Figure 1
 pdf("/Volumes/share/mnt/Data0/PROJECTS/CROPSeq/IV/dCas9-KRAB.pdf")
 plot(log2(y) ~ log2(x),xlab="NHA:log2(TPM)", ylab="NHA-dCas9-KRAB:log2(TPM)",
      pch=20, col="grey", 
